d4eb726ad8ccdc99ae29201a0f8fab0c7f1a45cf
Christopher Basketfield <chris.basketfield@gmail.com>
2019-10-20 17:48:24 -0800

    moved problem serving logic to a function

--- 1-quiz-game/main.go
+++ 1-quiz-game/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"fmt";
-	"flag";
-	"os";
-	"encoding/csv";
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
 	"strings"
 )
 
-func checkErr(err error,msg string){
+func checkErr(err error, msg string) {
 	if err != nil {
 		fmt.Printf(msg)
 		os.Exit(1)
@@ -17,42 +17,47 @@ func checkErr(err error,msg string){
 
 type problem struct {
 	question string
-	answer string
+	answer   string
 }
 
 func parseProblems(lines [][]string) []problem {
 	returnValue := make([]problem, len(lines))
-	for i,line := range lines {
+	for i, line := range lines {
 		returnValue[i] = problem{
 			question: strings.TrimSpace(line[0]),
-			answer: strings.TrimSpace(line[1]),
+			answer:   strings.TrimSpace(line[1]),
 		}
 	}
 	return returnValue
 }
 
-func main(){
-	csvFileName := flag.String("csv","problems.csv","csv question,answer pairs")
+func serveProblem(i int, problem *problem) bool {
+	fmt.Printf("Problem #%d: %s = ", i+1, problem.question)
+	var answer string
+	fmt.Scanf("%s\n", &answer)
+	return answer == problem.answer
+}
+
+func main() {
+	csvFileName := flag.String("csv", "problems.csv", "csv question,answer pairs")
 	flag.Parse()
 
 	file, err := os.Open(*csvFileName)
-	checkErr(err,fmt.Sprintf("Failed to open file: %s\n", *csvFileName))
-	
+	checkErr(err, fmt.Sprintf("Failed to open file: %s\n", *csvFileName))
+
 	r := csv.NewReader(file)
-	lines,err := r.ReadAll()
-	checkErr(err,fmt.Sprintf("Failed to parse file: %s",*csvFileName))
+	lines, err := r.ReadAll()
+	checkErr(err, fmt.Sprintf("Failed to parse file: %s", *csvFileName))
 
 	problems := parseProblems(lines)
 
 	correct := 0
-	for i,problem := range problems{
-		fmt.Printf("Problem #%d: %s = ",i+1,problem.question)
-		var answer string
-		fmt.Scanf("%s", &answer)
-		if answer == problem.answer {
+	for i, problem := range problems {
+		outcome := serveProblem(i, &problem)
+		if outcome {
 			correct++
 		}
 	}
 
-	fmt.Printf("Score: %d/%d\n",correct,len(problems))
+	fmt.Printf("Score: %d/%d", correct, len(problems))
 }
